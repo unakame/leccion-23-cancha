d24f8421b55dc8546f4cbbbea387d8d6826bc959
Dana Franco <turquoise.ilg@hotmail.com>
2017-04-20 11:11:25 -0500

    setting circle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,12 @@
 var fondo = document.createElement("div");
             document.body.appendChild(fondo);
-            fondo.setAttribute("id","cancha");
+            fondo.setAttribute("id","field");
 
 
 var borde = document.createElement("div");
             document.body.appendChild(borde);
-            borde.setAttribute("id","cuadro");
-            
+            borde.setAttribute("id","square");
+
+var circulo = document.createElement("div");
+              document.body.appendChild(circulo);
+              circulo.setAttribute("id","circle")
