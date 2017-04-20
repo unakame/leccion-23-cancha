aa7c5ad9e9ecfc06705451714756d6d69659080a
Dana Franco <turquoise.ilg@hotmail.com>
2017-04-20 10:37:20 -0500

    setting the biggest white block

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 var fondo = document.createElement("div");
             document.body.appendChild(fondo);
             fondo.setAttribute("id","cancha");
+
+
+var borde = document.createElement("div");
+            document.body.appendChild(borde);
+            borde.setAttribute("id","cuadro");
+            
