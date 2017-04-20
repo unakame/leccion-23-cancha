bfa8cc5d6fdb6e1f1ab8384fdc56a60982379e74
Dana Franco <turquoise.ilg@hotmail.com>
2017-04-20 12:26:49 -0500

    indenting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,10 @@
-var fondo = document.createElement("div");
-            document.body.appendChild(fondo);
-            fondo.setAttribute("id","field");
+var fondo  =  document.createElement("div");
+              document.body.appendChild(fondo);
+              fondo.setAttribute("id","field");
 
-
-var borde = document.createElement("div");
-            document.body.appendChild(borde);
-            borde.setAttribute("id","square");
+var borde   = document.createElement("div");
+              document.body.appendChild(borde);
+              borde.setAttribute("id","square");
 
 var circulo = document.createElement("div");
               document.body.appendChild(circulo);
@@ -19,24 +18,24 @@ var cuadro2 = document.createElement("div");
               document.body.appendChild(cuadro2);
               cuadro2.setAttribute("id","square2");
 
-var esfera1= document.createElement("div");
-             document.body.appendChild(esfera1);
-             esfera1.setAttribute("class","ball");
-             esfera1.setAttribute("id","ball1");
+var esfera1 = document.createElement("div");
+              document.body.appendChild(esfera1);
+              esfera1.setAttribute("class","ball");
+              esfera1.setAttribute("id","ball1");
 
-var esfera2= document.createElement("div");
-             document.body.appendChild(esfera2);
-             esfera2.setAttribute("class","ball");
-             esfera2.setAttribute("id","ball2");
+var esfera2 = document.createElement("div");
+              document.body.appendChild(esfera2);
+              esfera2.setAttribute("class","ball");
+              esfera2.setAttribute("id","ball2");
 
-var esfera3= document.createElement("div");
+var esfera3 = document.createElement("div");
               document.body.appendChild(esfera3);
               esfera3.setAttribute("class","ball");
               esfera3.setAttribute("id","ball3");
 
-var texto = document.createElement("p");
-            document.body.appendChild(texto);
-            texto.setAttribute("id","phrase");
-            
-var labo=   document.createTextNode("Laboratoria 2017");
-            texto.appendChild(labo);
+var texto   = document.createElement("p");
+              document.body.appendChild(texto);
+              texto.setAttribute("id","phrase");
+
+var labo    = document.createTextNode("Laboratoria 2017");
+              texto.appendChild(labo);
